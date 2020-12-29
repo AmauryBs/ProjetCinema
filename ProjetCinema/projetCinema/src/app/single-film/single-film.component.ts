6098731e5539c72a459a6468a5de4f38913208c6
Amaury <43411870+AmauryBs@users.noreply.github.com>
2020-12-29 21:50:08 +0100

    single film list component

--- ProjetCinema/projetCinema/src/app/single-film/single-film.component.ts
+++ ProjetCinema/projetCinema/src/app/single-film/single-film.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Subscription } from 'rxjs';
+import { Acteur } from '../models/acteur.model';
 import { Film } from '../models/film.model';
+import { Personnage } from '../models/personnage.model';
 import { ActeurService } from '../services/acteur.service';
 import { FilmService } from '../services/film.service';
 import { PersonnageService } from '../services/personnage.service';
@@ -22,12 +24,13 @@ export class SingleFilmComponent implements OnInit {
 
   ngOnInit() {
     const id = this.route.snapshot.params['id'];
-    this.film = this.filmService.getFilmById(+id).then( res =>{
-      console.log(res)
-      return new Film(res[0][0],res[0][1],res[0][3],res[0][5],res[0][2], res[0][4],res[0][7],res[0][6]) })
-    this.persos = this.personnageService.getPersoByFilm(id)
+    this.film = this.filmService.getFilmById(+id).then( (res:Film) =>{
+      res.dateSortie = new Date(res.dateSortie)
+      return res})
+    this.persos = this.personnageService.getPersoByFilm(+id).then( (res:Personnage) =>{
+      return res})
     for(var perso of this.persos){
-      const res = this.acteurService.getActeurById(perso.noAct).then(res=>{return res[0][1] + " " + res[0][2];})
+      const res = this.acteurService.getActeurById(perso.noAct).then((res:Acteur)=>{return res.prenAct + " " + res.nomAct;})
       perso["nomAct"] = res
     }
   }
